2b21c0eb47e216bb3d9db2ff1eb27feb3b990fd2
KevR98 <ramil.kevin98@gmail.com>
2025-11-20 18:21:30 +0100

    fixed

--- src/main/java/kevinramil/StreetSell/Payloads/UtenteDTO.java
+++ src/main/java/kevinramil/StreetSell/Payloads/UtenteDTO.java
@@ -7,10 +7,10 @@ import jakarta.validation.constraints.Size;
 public record UtenteDTO(
         // Username
         @NotEmpty(message = "Il campo username è obbligatorio.")
-        @Size(min = 3, max = 10, message = "username deve avere almeno 3 caratteri e max 10")
+        @Size(min = 3, max = 20, message = "username deve avere almeno 3 caratteri e max 20")
         String username,
 
-        
+
         // Email
         @NotEmpty(message = "Il campo email è obbligatorio.")
         @Email(message = "L'email inserita non ha un formato valido.")
